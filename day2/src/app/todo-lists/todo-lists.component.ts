33e729f213be3a01a77b86f7948498ac4e6931d0
Kevin Cunningham <github@kevincunningham.co.uk>
2023-02-07 14:12:12 +0000

    adds metric units

--- day2/src/app/todo-lists/todo-lists.component.ts
+++ day2/src/app/todo-lists/todo-lists.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RestService, Todo } from '../rest.service';
 
 @Component({
   selector: 'app-todo-lists',
   templateUrl: './todo-lists.component.html',
   styleUrls: ['./todo-lists.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoListsComponent {
   public todos: Todo[] = [];
   constructor(private todoService: RestService) {}
 
   ngOnInit() {
+    console.log(this.todos);
     this.todoService.getTodos().subscribe({
       next: (response) => {
-        this.todos = response;
+        this.todos = [];
+        this.todos = [...response];
         // multiple statements
+        console.log(this.todos);
       },
       error: (error) => {
         console.error(error);
